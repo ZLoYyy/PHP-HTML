c83a8a02d091e88cf707ff0761c85ff58d87ccaa
ZLoYyy <qzloyp@gmail.com>
2016-09-10 23:53:17 +0900

    call db_connect function

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     require_once("database.php");
     require_once("models/articles.php");
     
-    $articles = articles_ALL();
+    $link = db_connect();
+    $articles = articles_ALL($link);
 
     include("views/articles.php");
 

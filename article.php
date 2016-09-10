f301620c1750c39669e3d064cd64b842dbf24232
ZLoYyy <qzloyp@gmail.com>
2016-09-11 00:05:35 +0900

    a datebase connection

--- article.php
+++ article.php
@@ -1,8 +1,9 @@
 <?php
     require_once("database.php");
     require_once("models/articles.php");
-    
-    $article = articles_Get($_GET['id']);
+
+    $link = db_connect(); //Соединение с БД
+    $article = articles_Get($link, $_GET['id']);
 
     include("views/article.php");
 

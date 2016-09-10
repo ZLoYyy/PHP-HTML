7f33499da4ddbac7998ebdb54f165d4fb7411786
ZLoYyy <qzloyp@gmail.com>
2016-09-10 23:51:18 +0900

    correction

--- database.php
+++ database.php
@@ -6,7 +6,7 @@ define('MYSQL_DB', 'blog');
 
 function db_connect()
 {
-    $link = mysql_connect(MYSQL_SERVER, MYSQL_USER,                                   MYSQL_PASSWORD, MYSQL_DB)
+    $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB)
     or die("Error: ".mysqli_error($link));
     if(!mysqli_set_charset($link, "utf8"))
         {

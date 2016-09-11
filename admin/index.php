a3c1021509258a155cb58d8f8efd10a749a7e503
ZLoYyy <qzloyp@gmail.com>
2016-09-11 19:40:02 +0900

    Connection database and presentation

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "Admin Panel";
+#Подключаем файлы
+require_once("../database.php");
+require_once("../models/articles.php");
+
+#Соединение с БД
+$link = db_connect();
+
+
+$articles = articles_ALL($link);
+
+#Подключение шаблона
+include("../views/articles_admin.php");
 
 ?>

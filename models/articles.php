8a6710ec086845073f87febda05ea0d61b165007
ZLoYyy <qzloyp@gmail.com>
2016-09-14 19:35:33 +0900

    adding articles

--- models/articles.php
+++ models/articles.php
@@ -37,7 +37,27 @@ function articles_GET($link, $id_article)
     return["id" => 1, "title" => "Заголовок", "date" => "10-09-2016", "content" => "Скоро тут будет статья"];
 }
 function articles_NEW($title, $date, $content)
-{}
+{
+    //Подготовка
+    $title = trim($title);
+    $content - trim($content);
+    
+    //Проверка
+    if($title == "")
+        return false;
+    
+    //Запрос
+    $t = "INSERT INTO articles (title, date, content) VALUES ('%s','%s', '%s')";
+    
+    $query = sprintf($t, mysqli_real_escape_string($link, $title), mysqli_real_escape_string($link, $date), mysqli_real_escape_string($link, $content));
+    
+    echo $query;
+    $result = mysqli_query($link, $query);
+    
+    if(!$result)
+        die(mysqli_error($link));
+    return true;
+}
 function articles_EDIT($id, $title, $date, $content)
 {}
 function articles_DELETE($id)

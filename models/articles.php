54aba7adfc39290605755c0894cc909c2183e5fc
ZLoYyy <qzloyp@gmail.com>
2016-09-23 17:43:14 +0900

    edit and delete

--- models/articles.php
+++ models/articles.php
@@ -21,6 +21,7 @@ function articles_ALL($link)
     return $articles;
     
 }
+
 function articles_GET($link, $id_article)
 {
     //Запрос
@@ -36,7 +37,8 @@ function articles_GET($link, $id_article)
     
     return["id" => 1, "title" => "Заголовок", "date" => "10-09-2016", "content" => "Скоро тут будет статья"];
 }
-function articles_NEW($title, $date, $content)
+
+function articles_NEW($link, $title, $date, $content)
 {
     //Подготовка
     $title = trim($title);
@@ -58,8 +60,51 @@ function articles_NEW($title, $date, $content)
         die(mysqli_error($link));
     return true;
 }
-function articles_EDIT($id, $title, $date, $content)
-{}
-function articles_DELETE($id)
-{}
+function articles_EDIT($link, $id, $title, $date, $content)
+{
+     $title = trim($title);
+        $content = trim($content);
+        $date = trim($date);
+        $id = (int)$id;
+            
+        // Проверка
+        if ($title == '')
+            return false;
+        
+        // Запрос
+        $template_update = "UPDATE articles SET title='%s', content='%s', date='%s' WHERE id='%d'";
+            
+        $query = sprintf($template_update, 
+                         mysqli_real_escape_string($link, $title),
+                         mysqli_real_escape_string($link, $content),
+                         mysqli_real_escape_string($link, $date),
+                         $id);
+        
+        $result = mysqli_query($link, $query);
+        
+        if (!result)
+            die(mysqli_error($link));
+        
+        return mysqli_affected_rows($link);
+}
+function articles_DELETE($link, $id)
+{
+    $id = (int)$id;
+    //Проверка
+    if($id == 0)
+        return false;
+    //Запрос
+    $query = sprintf("DELETE FROM articles WHERE id='id'", $id);
+    $result = mysqli_query($link, $query);
+    
+    if(!$result)
+        die(mysqli_error($link));
+    
+    return mysqli_affected_rows($link);
+}
+function articles_intro($text, $len = 255)
+    {
+        return mb_substr($text, 0, $len);
+    }
+    
 ?>
